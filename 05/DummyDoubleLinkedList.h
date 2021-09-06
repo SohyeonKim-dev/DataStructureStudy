a8356d0c8e2ed33b563e966b24a81b71ad94de76
SoHyeon Kim <happythgus89@gmail.com>
2021-09-06 11:41:51 +0900

    Add files via upload

---
+++ 05/DummyDoubleLinkedList.h
@@ -0,0 +1,33 @@
+#ifndef __DUMMY_DOUBLE_LINKED_LIST__
+#define __DUMMY_DOUBLE_LINKED_LIST__
+
+#define TRUE		1
+#define FALSE		0
+
+typedef int Data;
+
+typedef struct _node {
+	Data data;
+	struct _node* next;
+	struct _node* prev;
+} Node;
+
+typedef struct _dummyDoubleLinkedList {
+	Node* head;
+	Node* tail;
+	Node* cur;
+	int numOfData;
+} DummyDoubleLinkedList;
+
+typedef DummyDoubleLinkedList List;
+
+void ListInit(List* plist);
+void LInsert(List* plist, Data data);
+
+int LFirst(List* plist, Data * pdata);
+int LNext(List* plist, Data * pdata);
+
+Data LRemove(List* plist);
+int LCount(List* plist);
+
+#endif __DUMMY_DOUBLE_LINKED_LIST__
