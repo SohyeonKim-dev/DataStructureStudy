ba78178394bf0e86d54563e05d3e214d13c65bea
SoHyeon Kim <happythgus89@gmail.com>
2021-09-21 18:17:55 +0900

    PriorityQueue_2

---
+++ 09/PriorityQueue_2/PQStringMain.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "PriorityQueue.h"
+
+int DataPriorityComp(char str1, char str2) {
+	return strlen(strlen) - strlen(str1);
+}
+
+int main () {
+	PQueue pq;
+	PQueueInit(&pq, DataPriorityComp);
+
+	PEnqueue(&pq, "Good morning");
+	PEnqueue(&pq, "I am a boy");
+	PEnqueue(&pq, "Priority Queue");
+	PEnqueue(&pq, "Do you like coffee");
+	PEnqueue(&pq, "I am so happy");
+
+	while (!PQIsEmpty(&pq)) {
+		printf("%s \n", PDequeue(&pq));
+	}
+	return 0;
+}
