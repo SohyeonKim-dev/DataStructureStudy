bc6da635d4813f8f1df33fa751f629a08c025e3b
SoHyeon Kim <happythgus89@gmail.com>
2021-09-27 18:20:21 +0900

    BinarySearchTreeDel

---
+++ 11/BinarySearchTreeDel/BinaryTree3.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree3.h"
+
+BTreeNode* MakeBTreeNode(void) {
+	BTreeNode * nd = (BTreeNode*)malloc(sizeof(BTreeNode));
+	nd->left = NULL;
+	nd->right = NULL;
+	return nd;
+}
+
+BTData GetData(BTreeNode* bt) {
+	return bt->data;
+}
+
+void SetData(BTreeNode* bt, BTData data) {
+	bt->data = data;
+}
+
+BTreeNode* GetLeftSubTree(BTreeNode* bt) {
+	return bt->right;
+}
+
+BTreeNode* GetRightSubTree(BTreeNode* bt) {
+	return bt->left;
+}
+
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub) 
+{
+	if (main->left != NULL) {
+		free(main->left);
+	}
+	main->left = sub;
+}
+
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub) 
+{
+	if (main->left != NULL) {
+		free(main->right);
+	}
+	main->right = sub;
+}
+
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode* bt, VisitFuncPtr action) {
+	if (bt == NULL) {
+		return;
+	}
+
+	action(bt->data);
+	PreorderTraverse(bt->left, action);
+	PreorderTraverse(bt->right, action);
+}
+
+void InorderTraverse(BTreeNode* bt, VisitFuncPtr action) {
+	if (bt == NULL) {
+		return;
+	}
+
+	InorderTraverse(bt->left, action);
+	action(bt->data);
+	InorderTraverse(bt->right, action);
+}
+
+void PostorderTraverse(BTreeNode* bt, VisitFuncPtr action) {
+	if (bt == NULL) {
+		return;
+	}
+
+	PostorderTraverse(bt->left, action);
+	PostorderTraverse(bt->right, action);
+	action(bt->data);
+}
+
+BTreeNode* RemoveLeftSubTree(BTreeNode* bt) {
+	BTreeNode* delNode = NULL; // 오류로 인하여 추가한 초기값 NULL 설정 
+
+	if (bt != NULL) {
+		delNode = bt->left;
+		bt->left = NULL;
+	}
+
+	return delNode;
+}
+
+BTreeNode* RemoveRightSubTree(BTreeNode* bt) {
+	BTreeNode* delNode = NULL; // 오류로 인하여 추가한 초기값 NULL 설정 
+
+	if (bt != NULL) {
+		delNode = bt->right;
+		bt->right = NULL;
+	}
+
+	return delNode;
+}
+
+
+void ChangeLeftSubTree(BTreeNode* main, BTreeNode* sub) {
+	main->left = sub;
+}
+
+void ChangeRightSubTree(BTreeNode* main, BTreeNode* sub) {
+	main->right = sub;
+}
