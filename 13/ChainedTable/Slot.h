dcbe7efe578af12bd10a98664b8cf9bd7da5ed32
SoHyeon Kim <happythgus89@gmail.com>
2021-09-29 01:24:40 +0900

    ChainedTable

---
+++ 13/ChainedTable/Slot.h
@@ -0,0 +1,16 @@
+#ifndef __SLOT_H__
+#define __SLOT_H__
+
+#include "Person.h"
+
+typedef int Key;
+typedef Person* Value;
+
+typedef struct _slot {
+	Key key;
+	Value val;
+}Slot;
+
+#endif __SLOT_H__
+
+
